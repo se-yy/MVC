d5864ac73d81b612baff200589ca07aa593b27ea
Seyoung Shin <pipixpfjs@gmail.com>
2022-06-07 10:34:08 +0900

    Spring WEB MVC 동작

--- SpringMVC01/src/main/java/kr/bit/model/MemberDAO.java
+++ SpringMVC01/src/main/java/kr/bit/model/MemberDAO.java
@@ -9,20 +9,15 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberDAO {
-	private static SqlSessionFactory sqlSessionFactory;
 	
-	// 초기화 블럭 - 프로그램 실행 시 딱 한번만 실행되는 코드 영역
-	static {
-		try {
-			String resource = "kr/bit/mybatis/config.xml";
-			InputStream inputStream = Resources.getResourceAsStream(resource); // xml 읽기(Input)
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream); // connection pool 생성
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
 	
 	// 회원 전체 리스트 보기
 	public List<MemberVO> memberList() {
--- SpringMVC01/src/main/java/kr/narp/myapp/MemberController.java
+++ SpringMVC01/src/main/java/kr/narp/myapp/MemberController.java
@@ -1,14 +1,28 @@
 package kr.narp.myapp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.bit.model.MemberDAO;
+import kr.bit.model.MemberVO;
 
 @Controller
 public class MemberController {
 	
+	@Autowired
+	private MemberDAO dao;
+	
 	@RequestMapping("/memberList.do")
-	public String memberList() {
+	public String memberList(Model model) {
+		// MemberDAO dao = new MemberDAO(); -> 의존성 지양
+		List<MemberVO> list = dao.memberList();
 		
+		// 객체바인딩
+		model.addAttribute("list", list);
 		
 		return "memberList";
 	}
