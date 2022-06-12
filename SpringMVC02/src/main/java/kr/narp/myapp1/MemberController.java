f47734f50b7fa91990ba735921445968aa631ec4
Seyoung Shin <pipixpfjs@gmail.com>
2022-06-12 21:53:42 +0900

    Mapper 인터페이스와 XML mapper 파일 이용 CRUD하기

--- SpringMVC02/src/main/java/kr/narp/myapp1/MemberController.java
+++ SpringMVC02/src/main/java/kr/narp/myapp1/MemberController.java
@@ -8,18 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import kr.bit.model.MemberDAO;
+import kr.bit.mapper.MemberMapper;
 import kr.bit.model.MemberVO;
 // @ ->어노테이션(전처리) -> Spring Container에서 관리를 해준다.
 @Controller
 public class MemberController {
     
 	@Autowired
-	private MemberDAO dao; // DI
+	//private MemberDAO dao; // DI
+	private MemberMapper memberMapper;
 	
 	@RequestMapping("/memberList.do")
 	public String memberList(Model model) { // HttpServletRequest == Model
-		List<MemberVO> list=dao.memberList();
+		List<MemberVO> list=memberMapper.memberList();
 		// 객체바인딩
 		model.addAttribute("list", list);		
 		return "memberList"; 
@@ -28,7 +29,7 @@ public class MemberController {
 	@RequestMapping("/memberInsert.do")
 	public String memberInsert(MemberVO vo) { // 파라메터 수집(VO)
 		// 인코딩(한글깨지는 부분?) spring web mvc 한글 깨짐(구글검색)
-		int cnt=dao.memberInsert(vo);
+		int cnt=memberMapper.memberInsert(vo);
 				
 		return "redirect:/memberList.do";
 	}
@@ -41,7 +42,7 @@ public class MemberController {
 	
 	@RequestMapping("/memberDelete.do")
 	public String memberDelete(@RequestParam("num") int num) { // 파라메터를 수집 : num
-		int cnt=dao.memberDelete(num);
+		int cnt=memberMapper.memberDelete(num);
 		
 		return "redirect:/memberList.do";
 	}
@@ -49,7 +50,7 @@ public class MemberController {
 	@RequestMapping("/memberContent.do")
 	public String memberContent(int num, Model model) {
 		
-		MemberVO vo=dao.memberContent(num);
+		MemberVO vo=memberMapper.memberContent(num);
 		//객체바인딩
 		model.addAttribute("vo", vo);
 		
@@ -59,7 +60,7 @@ public class MemberController {
 	@RequestMapping("/memberUpdate.do")
 	public String memberUpdate(MemberVO vo) {
 		
-		int cnt=dao.memberUpdate(vo);
+		int cnt=memberMapper.memberUpdate(vo);
 		
 		return "redirect:/memberList.do";
 	}
