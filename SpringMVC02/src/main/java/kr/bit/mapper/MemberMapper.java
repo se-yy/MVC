c78be7743fa55503e24128f7e2f904e63516c867
Seyoung Shin <pipixpfjs@gmail.com>
2022-06-12 22:37:12 +0900

    Mapper 인터페이스와 @(Annotation)이용 CRUD하기

--- SpringMVC02/src/main/java/kr/bit/mapper/MemberMapper.java
+++ SpringMVC02/src/main/java/kr/bit/mapper/MemberMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import kr.bit.model.MemberVO;
 
 // SQL id = "메서드 이름"
-@Mapper
+//@Mapper
 public interface MemberMapper {
 	public List<MemberVO> memberList();
 	public int memberInsert(MemberVO vo);
@@ -15,3 +15,33 @@ public interface MemberMapper {
 	public MemberVO memberContent(int num);
 	public int memberUpdate(MemberVO vo);
 }
+
+/*
+// annotation 이용한 방법
+public interface MemberMapper {
+	@Select("select * from member")
+	public List<MemberVO> memberList();
+	
+	@Insert("insert into member(id, pass, name, age, email, phone)
+        values(
+        #{id},
+        #{pass},
+        #{name},
+        #{age},
+        #{email},
+        #{phone}
+        ) ")
+	public int memberInsert(MemberVO vo);
+	
+	@Delete("delete from member where num=#{num}")
+	public int memberDelete(int num);
+	
+	@Select("select * from member where num=#{num}")
+	public MemberVO memberContent(int num);
+	
+	@Update("update member 
+         set age=#{age}, email=#{email}, phone=#{phone} 
+         where num=#{num}")
+	public int memberUpdate(MemberVO vo);
+}
+*/
